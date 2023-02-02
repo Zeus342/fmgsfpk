e0aae341a764eacc66d3c75340e9e26dd8fee435
Franz Röhrle <74118590+Zeus342@users.noreply.github.com>
2023-02-02 13:39:48 +0100

    Home Page Completion

--- serverRoot.py
+++ serverRoot.py
@@ -4,8 +4,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route("/projekte")
+def projekte():
+    return render_template('projekte.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
