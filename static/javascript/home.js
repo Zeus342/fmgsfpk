60deb26d46bc45cb4dd7c0aaffd1038ef6243fa9
Franz Röhrle <74118590+Zeus342@users.noreply.github.com>
2023-02-06 19:57:56 +0100

    Projects page and unserprojekt page updates

--- static/javascript/home.js
+++ static/javascript/home.js
@@ -1,17 +1,10 @@
-function showHideUnserProjekt() {
-    var x = document.getElementById("unserProjektDiv");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-  }
-
-var projectButtonActiveClass = document.getElementById("projectButton").className
 var homeButtonActiveClass = document.getElementById("homeButton").className
+var projectsButtonActiveClass = document.getElementById("projectButton").className
 var unserProjektActiveClass = document.getElementById("unserProjektButton").className
 var headerActiveClass = document.getElementById("header").className
 var buttonsActiveClass = document.getElementById("buttons").className
+var projectsContentActiveClass = document.getElementById("projectsContent").className
+var unserProjektContentActiveClass = document.getElementById("unserProjektContent").className
 
 function atHome() {
     document.getElementById("homeButton").className = "activeButton";
@@ -20,45 +13,37 @@ function atHome() {
 
 function makeHomeActiveButton() {
     document.getElementById("homeButton").className = "activeButton";
-
+    document.getElementById("projectButton").className = "navButton";
+    document.getElementById("unserProjektButton").className = "navButton";
+    document.getElementById("projectsContent").className = "hideProjectsContent";
+    document.getElementById("unserProjektContent").className = "hideUnserProjektContent";
 
     if (buttonsActiveClass === "moveUp");
         document.getElementById("header").className = "moveDown";
-        document.getElementById("buttons").className = "moveDown";
-
-    if (projectButtonActiveClass === "activeButton");
-        document.getElementById("projectButton").className = "navButton";
-        document.getElementById("unserProjektButton").className = "navButton";
-
-    if (unserProjektActiveClass === "activeButton");
-        document.getElementById("projectButton").className = "navButton";
-        document.getElementById("unserProjektButton").className = "navButton";
+        document.getElementById("buttons").className = "moveDown";     
 }
 function makeProjectsActiveButton() {
     document.getElementById("projectButton").className = "activeButton";
+    document.getElementById("homeButton").className = "navButton";
+    document.getElementById("unserProjektButton").className = "navButton";
     document.getElementById("homeButton").disabled = false;
     document.getElementById("header").className = "moveUp";
     document.getElementById("buttons").className = "moveUp";
+    document.getElementById("projectsContent").className = "showProjectsContent";
 
-    if (homeButtonActiveClass === "activeButton");
-        document.getElementById("homeButton").className = "navButton";
-        document.getElementById("unserProjektButton").className = "navButton";
+    if (unserProjektContentActiveClass === "showUnserProjektContent");
+        document.getElementById("unserProjektContent").className = "hideUnserProjektContent";
 
-    if (unserProjektActiveClass === "activeButton");
-        document.getElementById("homeButton").className = "navButton";
-        document.getElementById("unserProjektButton").className = "navButton";
 }
 function makeUnserProjektActiveButton() {
     document.getElementById("unserProjektButton").className = "activeButton";
+    document.getElementById("homeButton").className = "navButton";
+    document.getElementById("projectButton").className = "navButton";
     document.getElementById("homeButton").disabled = false;
     document.getElementById("header").className = "moveUp";
     document.getElementById("buttons").className = "moveUp";
+    document.getElementById("unserProjektContent").className = "showUnserProjektContent";
 
-    if (homeButtonActiveClass === "activeButton");
-        document.getElementById("homeButton").className = "navButton";
-        document.getElementById("projectButton").className = "navButton";
-
-    if (projectButtonActiveClass === "activeButton");
-        document.getElementById("homeButton").className = "navButton";
-        document.getElementById("projectButton").className = "navButton";
+    if (projectsContentActiveClass === "showProjectsContent");
+        document.getElementById("projectsContent").className = "hideProjectsContent";
 }
