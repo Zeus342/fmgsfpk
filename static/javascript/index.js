89d3e5913bf5c45218b3a1b367d595ef1180c571
Franz Röhrle <74118590+Zeus342@users.noreply.github.com>
2023-02-26 20:00:21 +0100

    Projects Page Development
    FINISHED

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -9,7 +9,7 @@ function makeUnserProjektActiveButton() {
     document.getElementById("unserProjektButton").className = "activeButton";
 
     if (projekteButtonClass === "activeButton");
-        document.getElementById("projekteButton").className ="navButton"; 
+        document.getElementById("projekteButton").className ="navButton";
 }
 
 function makeProjekteActiveButton() {
@@ -19,7 +19,7 @@ function makeProjekteActiveButton() {
     document.getElementById("pxButtons").classList.remove("hidden");
     document.getElementById("pxButtons").classList.add("moveIn");
     document.getElementById("pxButtons").classList.remove("moveOut");
-
+    document.getElementById("selectProjektkursPromt").classList.remove("hidden");
 
     if (unserProjektButtonClass === "activeButton");
     document.getElementById("unserProjektButton").className = "navButton";
@@ -27,11 +27,65 @@ function makeProjekteActiveButton() {
 
 function backButton() {
     document.getElementById("projekteButton").className = "navButton";
+    document.getElementById("pxm").className = "navButton";
+    document.getElementById("pxbc").className = "navButton";
+    document.getElementById("pxkure").className = "navButton";
+    document.getElementById("pxgep").className = "navButton";
     document.getElementById("unserProjektButton").className = "activeButton";
+    document.getElementById("selectProjektkursPromt").classList.add("hidden");
+    document.getElementById("pxmProjekte").classList.add("hidden");
+    document.getElementById("pxbcProjekte").classList.add("hidden");
+    document.getElementById("pxkureProjekte").classList.add("hidden");
+    document.getElementById("pxgepProjekte").classList.add("hidden");
+
     if (projekteButtonClass === "activeButton");
         document.getElementById("menuButtons").classList.add("moveIn");
         document.getElementById("menuButtons").classList.remove("moveOut");
         document.getElementById("pxButtons").classList.remove("moveIn");
-        document.getElementById("pxButtons").classList.add("moveOut");
-    
+        document.getElementById("pxButtons").classList.add("moveOut"); 
+}
+
+function showPxmProjekte() {
+    document.getElementById("pxbc").classList = "navButton";
+    document.getElementById("pxkure").classList = "navButton";
+    document.getElementById("pxgep").classList = "navButton";
+    document.getElementById("pxgepProjekte").classList.add("hidden");
+    document.getElementById("pxbcProjekte").classList.add("hidden");
+    document.getElementById("pxkureProjekte").classList.add("hidden");
+    document.getElementById("pxm").classList = "activeButton";
+    document.getElementById("selectProjektkursPromt").classList.add("hidden");
+    document.getElementById("pxmProjekte").classList.remove("hidden");
+}
+function showPxbcProjekte() {
+    document.getElementById("pxm").classList = "navButton";
+    document.getElementById("pxkure").classList = "navButton";
+    document.getElementById("pxgep").classList = "navButton";
+    document.getElementById("pxmProjekte").classList.add("hidden");
+    document.getElementById("pxgepProjekte").classList.add("hidden");
+    document.getElementById("pxkureProjekte").classList.add("hidden");
+    document.getElementById("pxbc").classList = "activeButton";
+    document.getElementById("selectProjektkursPromt").classList.add("hidden");
+    document.getElementById("pxbcProjekte").classList.remove("hidden");
+}
+function showPxkureProjekte() {
+    document.getElementById("pxm").classList = "navButton";
+    document.getElementById("pxbc").classList = "navButton";
+    document.getElementById("pxgep").classList = "navButton";
+    document.getElementById("pxmProjekte").classList.add("hidden");
+    document.getElementById("pxbcProjekte").classList.add("hidden");
+    document.getElementById("pxgepProjekte").classList.add("hidden");
+    document.getElementById("pxkure").classList = "activeButton";
+    document.getElementById("selectProjektkursPromt").classList.add("hidden");
+    document.getElementById("pxkureProjekte").classList.remove("hidden");
+}
+function showPxgepProjekte() {
+    document.getElementById("pxm").classList = "navButton";
+    document.getElementById("pxbc").classList = "navButton";
+    document.getElementById("pxkure").classList = "navButton";
+    document.getElementById("pxmProjekte").classList.add("hidden");
+    document.getElementById("pxbcProjekte").classList.add("hidden");
+    document.getElementById("pxkureProjekte").classList.add("hidden");
+    document.getElementById("pxgep").classList = "activeButton";
+    document.getElementById("selectProjektkursPromt").classList.add("hidden");
+    document.getElementById("pxgepProjekte").classList.remove("hidden");
 }
